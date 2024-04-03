8aafe1552be84c6a9f156eaf3af5c46cc2598b2e
yuanshuai <yuanshuai@gyyx.cn>
2024-04-03 15:53:43 +0800

    core

---
+++ rise-core/src/main/java/com/yuanshuaicn/RiseCoreApplication.java
@@ -0,0 +1,13 @@
+package com.yuanshuaicn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiseCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiseCoreApplication.class, args);
+	}
+
+}
---
+++ rise-core/src/test/java/com/yuanshuaicn/RiseCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanshuaicn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiseCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
