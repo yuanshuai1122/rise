76b6bd1137ee5d5efd39386a1a7c9e3397904902
andy <shuaiyuan1122@gmail.com>
2024-04-07 21:40:38 +0800

    internal

---
+++ rise-voice-conversion-service/src/main/java/com/yuanshuaicn/controller/InternalController.java
@@ -0,0 +1,21 @@
+package com.yuanshuaicn.controller;
+
+
+import com.yuanshuaicn.beans.common.ResultBean;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/internal")
+public class InternalController {
+
+
+//    public ResultBean<Object> callVoice4Text() {
+//
+//    }
+
+}
